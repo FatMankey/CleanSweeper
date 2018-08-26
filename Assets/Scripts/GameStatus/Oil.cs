7d27fddc5a5d0a41d51a4438986a6fff7f9eed56
Unknown <armandojarce@gmail.com>
2018-08-25 20:41:49 -0700

    init oil pickup
    works but needs to be worked on more.
    Deleted old scripts that were cluttering and unused.

---
+++ Assets/Scripts/GameStatus/Oil.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Oil : MonoBehaviour {
+    //  FUTURE STUB
+    //public GameObject[] AmountOfOil;
+    public Text ScoreBanner;
+    public int ValueOfOil = 0;
+    public int CurrentScore = 0;
+    private void Update() {
+        
+    ScoreBanner.text = "Score: " + CurrentScore.ToString();
+  
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Oil")) {
+            print("we got hit");
+            StartCoroutine(destroyOil(other.gameObject));
+        }
+    }
+
+    void ScoreUpdate(int newvalue) {
+        CurrentScore += newvalue;
+    }
+    IEnumerator destroyOil(GameObject others) {
+        yield return new WaitForSeconds(0.09f);
+        others.SetActive(false);
+        ScoreUpdate(ValueOfOil);
+    }
+}
