049dfd221e8f661a715315061893d08d4d90e579
Armando <armandojarce@gmail.com>
2018-09-18 11:47:54 -0700

    UI push

---
+++ Assets/SavingPrivateRyab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SavingPrivateRyab : MonoBehaviour
+{
+    public int currentlevels;
+
+    private void Awake()
+    {
+        LoadDate();
+    }
+
+    private void LoadDate()
+    {
+        if (File.Exists(Application.persistentDataPath + "/Levelinfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/Levelinfo.dat", FileMode.Open);
+            PlayerInfo PI = (PlayerInfo)bf.Deserialize(file);
+            file.Close();
+            currentlevels = PI.levels;
+        }
+    }
+}
